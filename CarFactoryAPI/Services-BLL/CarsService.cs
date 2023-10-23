78496018ca9a073749d42e853092ba95890f2edf
Abdalrahman Tarek <121307447+abdalrahmantarek@users.noreply.github.com>
2023-10-23 22:30:19 +0300

    Add project files.

---
+++ CarFactoryAPI/Services-BLL/CarsService.cs
@@ -0,0 +1,62 @@
+﻿using CarAPI.Entities;
+using CarAPI.Models;
+using CarAPI.Payment;
+using CarAPI.Repositories_DAL;
+
+namespace CarAPI.Services_BLL
+{
+    public class CarsService : ICarsService
+    {
+        private readonly ICarsRepository _carsRepository;
+		private readonly IOwnersRepository _ownerRepository;
+		private readonly ICashService _cashService;
+		public CarsService(ICarsRepository carsRepository, IOwnersRepository ownersRepository,
+			ICashService cashServic)
+        {
+            _carsRepository = carsRepository;
+            _ownerRepository = ownersRepository;
+            _cashService = cashServic;
+        }
+        public List<Car> GetAll()
+        {
+            var cars = _carsRepository.GetAllCars();
+            return cars;
+        }
+
+        public Car GetCarById(int id)
+        {
+            var car = _carsRepository.GetCarById(id);
+            /// Logic on the car
+            if (car == null) return null;
+            return car;
+        }
+
+        public bool AddCar(Car car)
+        {
+            return _carsRepository.AddCar(car);
+        }
+
+        public bool Remove(int carId)
+        {
+            return _carsRepository.Remove(carId);
+        }
+		public string BuyCar(BuyCarInput input)
+		{
+			var car = _carsRepository.GetCarById(input.CarId);
+			if (car == null)
+				return "Car doesn't exist";
+			if (car.Owner != null)
+				return "Already sold";
+			var owner = _ownerRepository.GetOwnerById(input.OwnerId);
+			if (owner == null)
+				return "Owner doesn't exist";
+			if (owner.Car != null)
+				return "solded";
+			owner.Car = car;
+			car.Owner = owner;
+
+			var paymentResult = _cashService.Pay(input.Amount);
+			return $"Successfull Car of Id: {input.CarId} is bought by {owner.Name} with payment result {paymentResult}";
+		}
+	}
+}
---
+++ CarsUnittest/CarServicesTests.cs
@@ -0,0 +1,83 @@
+﻿using CarAPI.Entities;
+using CarAPI.Models;
+using CarAPI.Payment;
+using CarAPI.Repositories_DAL;
+using CarAPI.Services_BLL;
+using CarFactoryAPITests.Fake;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace CarsUnittest
+{
+	public class CarServicesTests : IDisposable
+	{
+		private readonly ITestOutputHelper testOutput;
+		private Mock<IOwnersRepository> ownerRepoMock;
+		private Mock<ICarsRepository> carRepoMock;
+		private CarsService carsService;
+		public CarServicesTests(ITestOutputHelper testOutput) 
+		{
+		     this.testOutput = testOutput;
+			testOutput.WriteLine("StartTest");
+			carRepoMock = new Mock<ICarsRepository>();
+			ownerRepoMock = new Mock<IOwnersRepository>();
+			carsService = new CarsService(
+			   carRepoMock.Object,
+			   ownerRepoMock.Object,
+			   new CashService()
+			   );
+		}	
+		public void Dispose()
+		{
+			testOutput.WriteLine("start clean up");
+		}
+		[Fact]
+		public void BuyCar_CarHasOwner_AlreadySold()
+		{
+			CarsService carssService = new CarsService(
+				new StupCarsWithOwnerRepo(),
+				new StupOwnerWithoutCarRepo(),
+				new CashService()
+				);
+			BuyCarInput buyCarInput = new()
+			{
+				CarId = 2,
+				OwnerId = 1,
+				Amount = 100
+			};
+			var result = carsService.BuyCar(buyCarInput);
+			Assert.Equal("Car doesn't exist", result);
+			testOutput.WriteLine(" car is exist");
+		}
+
+
+		[Fact]
+		public void BuyCar_NewCarNewOwner_Sucessful()
+		{
+			Car car = new Car() { Id = 10, Price = 100 };
+			Owner owner = new Owner() { Id = 1, Name = "sayed" };
+			carRepoMock.Setup(m => m.GetCarById(It.IsAny<int>())).Returns(car);
+			ownerRepoMock.Setup(m => m.GetOwnerById(It.IsAny<int>())).Returns(owner);
+			BuyCarInput buyCarInput = new() { Amount = 100, CarId = 10, OwnerId = 2 };
+			var result = carsService.BuyCar(buyCarInput);
+			Assert.StartsWith("Successfull", result);
+		}
+		[Fact]
+		public void BuyCar_OwnerNotExist_NotExist()
+		{
+			Car car = new Car() { Id = 10, Price = 100 };
+			Owner owner = null;
+			carRepoMock.Setup(m => m.GetCarById(car.Id)).Returns(car);
+			ownerRepoMock.Setup(m => m.GetOwnerById(It.IsAny<int>())).Returns(owner);
+			BuyCarInput buyCarInput = new() { Amount = 100, CarId = 10, OwnerId = 2 };
+			var result = carsService.BuyCar(buyCarInput);
+			Assert.Equal("Owner doesn't exist", result);
+		}
+
+	}
+}
